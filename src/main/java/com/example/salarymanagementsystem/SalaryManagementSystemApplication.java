23058310e45ee5ce82d322a21a4cb5f7af135a77
thien <ctthien304@gmail.com>
2024-12-12 18:55:30 +0700

    test

---
+++ src/main/java/com/example/salarymanagementsystem/SalaryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.salarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salarymanagementsystem/SalaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
